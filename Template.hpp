d0dcb7d5510ec191403ddc442cc598fcf5837cbe
Mano Sriram <mano.sriram0@gmail.com>
2019-11-12 16:37:29 +0530

    Rewrite Template.hpp

--- Template.hpp
+++ Template.hpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <map>
 #include <math.h>
+#include <string.h>
 #include <algorithm>
+#include <vector>
+#include <float.h>
+#include <limits.h>
 #include <stack>
 #include <queue>
 #include "unordered_map"
-#define SPEED cin.sync_with_stdio(false);cin.tie(NULL);
-#define trace(x) cerr << #x << " " << x << '\n';
-#define ld long long
-#define disp(n) for (auto t : n)
-#define forn(t, n) for (ld t = 0; t < n; t++)
-#define pb push_back
-#define mp make_pair
-#define mod 1000000007
-#define c_sort(a) sort(a, a + sizeof(a) / sizeof(a[0]))
-#define NL cout << '\n';
-#define INF (int)1e9
 using namespace std;
+#define MAX INT_MAX
+#define MIN INT_MIN
+#define INF 1e9
+#define FASTER cin.sync_with_stdio(false);cin.tie(NULL);
+#define mp make_pair
+#define pb push_back
+#define ld long long int
 typedef vector<int> vi;
+typedef pair<int, int> pr;
+typedef map<int, int> mp;
 typedef unordered_map<int, int> ump;
