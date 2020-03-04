1a8eeb4f6b23f256976657063cb09d8bde5440ab
Mano Sriram <mano.sriram0@gmail.com>
2020-03-04 23:00:12 +0530

    Add new template.

---
+++ Template_New.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <math.h>
+#include "unordered_map"
+#define F first
+#define S second
+#define ll long long int
+#define pb push_back
+#define debug cout << "hi" << '\n';
+#define FASTER cin.sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+
+int main() {
+    FASTER;
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+#endif
+    int T;
+    cin >> T;
+    while (T--) {
+
+    }
+
+    debug;
+}
