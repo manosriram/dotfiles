12d740360b280e4338c866fe59edd9c92cdc3687
Mano Sriram <mano.sriram0@gmail.com>
2020-03-29 00:04:24 +0530

    Add Simple Template

---
+++ Simple.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <map>
+#define ll long long
+#define IOS cin.sync_with_stdio(false);cin.tie(nullptr);
+#define endl "\n"
+#define pb push_back
+const ll NN = 1e5+5;
+using namespace std;
+
+int main() {
+    IOS;
+}
+
