b09b4dd5ea914920c1d6677e7f2fbbced173b17c
Mano Sriram <mano.sriram@anatinuslabs.com>
2022-02-26 15:33:43 +0530

    Move from init.vim to lua based config

---
+++ lua/settings.lua
@@ -0,0 +1,82 @@
+local cmd = vim.cmd
+local g = vim.g
+
+function map(mode, shortcut, command, noremap)
+    vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = noremap, silent = true })
+end
+
+function nrmap(shortcut, command)
+    map('n', shortcut, command, false)
+end
+
+function nmap(shortcut, command)
+    map('n', shortcut, command, true)
+end
+
+function imap(shortcut, command)
+    map('i', shortcut, command, true)
+end
+
+-- Highlight coloring
+cmd('hi Visual cterm=NONE ctermbg=0 ctermfg=NONE')
+cmd('hi Search cterm=NONE ctermbg=0 ctermfg=white')
+cmd('hi Pmenu cterm=NONE ctermbg=white ctermfg=0')
+cmd('hi MatchParen cterm=NONE ctermbg=NONE ctermfg=green')
+cmd([[
+	set nocompatible              " be iMproved, required
+
+	filetype off                  " required
+	syntax on
+	set number
+	set laststatus=2
+	set noswapfile
+	set autoread
+	set noshowmode
+	set belloff=all
+	set noerrorbells
+	set visualbell
+	set t_vb=
+	filetype plugin on
+	set encoding=UTF-8
+	set bg=dark
+        set guicursor=i:hor25-iCursor
+        set guicursor+=n:block-Cursor/lCursor
+
+        colorscheme distinguished
+	hi Visual term=reverse cterm=reverse guibg=White
+	let base16colorspace=256
+
+	set backspace=indent,eol,start
+	set tabstop=4
+	set softtabstop=4 noexpandtab
+
+	set shiftwidth=4
+	set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
+	set splitbelow
+	set splitright
+	set relativenumber
+	set incsearch
+	set noerrorbells
+	set nowrap
+	set maxmempattern=20000
+	set ttimeout
+	set ttimeoutlen=100
+	set timeoutlen=3000
+        set t_Co=256
+        set termguicolors     " enable true colors support
+        highlight LineNr guifg=white
+        highlight Pmenu guibg=#2C3E50 gui=bold
+        highlight EndOfBuffer ctermfg=white
+]])
+
+g.go_auto_type_info = 1
+g.go_imports_mode = 'gopls'
+g.go_gopls_enabled = 1
+g.ycm_global_ycm_extra_conf = '~/.vim/.ycm_extra_conf.py'
+g.go_info_mode = 'guru'
+g.go_auto_type_info = 'gopls'
+g.NERDSpaceDelims = 1
+g.NERDTreeDirArrows = 0
+g.asduser_emmet_leader_key=','
+vim.api.nvim_set_keymap("n", "F", "<Plug>Lightspeed_S", { noremap = false, silent = true })
+vim.cmd("let g:lightline = {'colorscheme': 'simpleblack'}")
