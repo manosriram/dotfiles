e01a7e321715371d2cb547ab0774649db22cc469
Mano Sriram <mano.sriram0@gmail.com>
2024-04-11 01:06:41 +0530

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,31 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.font = wezterm.font('Fira Code', { weight = 'Medium' })
+config.color_scheme = '3024 Night'
+config.enable_tab_bar = true
+config.default_cursor_style = "SteadyUnderline"
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
+keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
+config.keys = keys
+
+-- and finally, return the configuration to wezterm
+return config
