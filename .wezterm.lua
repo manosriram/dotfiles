75c52436b6615e79f6be15b7fb84ceeab4346321
Mano Sriram <mano.sriram0@gmail.com>
2024-05-15 16:26:44 +0530

    minor fixes

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,13 +19,14 @@ config.enable_tab_bar = true
 config.default_cursor_style = "SteadyUnderline"
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
-keys = {
+local keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
     {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
     -- Make Option-Right equivalent to Alt-f; forward-word
     {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 }
 config.keys = keys
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
 
 -- and finally, return the configuration to wezterm
 return config
