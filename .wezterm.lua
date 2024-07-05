899de7286ab172748645cfeabd56caa53750cf29
Mano Sriram <mano.sriram0@gmail.com>
2024-07-06 03:32:50 +0530

    update wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -14,16 +14,29 @@ end
 
 -- For example, changing the color scheme:
 config.font = wezterm.font('Jetbrains Mono', { weight = 'Medium', italic = false })
+config.font_size = 13.0
 config.color_scheme = '3024 Night'
 config.enable_tab_bar = true
 config.default_cursor_style = "SteadyUnderline"
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 30
+config.color_scheme = 'Ef-Autumn'
+config.colors = {
+		background = 'rgba(0, 0, 0, 0.8)',
+}
+config.text_background_opacity = 0.7
 
 local keys = {
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
     {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
     -- Make Option-Right equivalent to Alt-f; forward-word
     {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  }
 }
 config.keys = keys
 config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
